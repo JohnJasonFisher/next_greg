ae78376be6e32d22590914689fdbdac4d22a771f
JohnJasonFisher <jjfisher91@gmail.com>
2022-01-01 18:44:11 -0600

    adds users pages and api

---
+++ pages/api/users/index.js
@@ -0,0 +1,6 @@
+export default function usersHandler(req, res) {
+    res.status(200).json({ users: [
+        { name: 'john' },
+        { name: 'greg' },
+    ] })
+  }
---
+++ pages/users/index.js
@@ -0,0 +1,8 @@
+export default function Users() {
+    return (
+    <div className="Users">
+        <h1>Users</h1>
+        <p>This is where all the users are listed</p>
+    </div>
+  )
+}
