8a4fcb779b69457b1a83bc200b6a6f72f0280aa7
JohnJasonFisher <jjfisher91@gmail.com>
2022-01-02 11:44:50 -0600

    fetch users data from api

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -2,5 +2,5 @@ export default function usersHandler(req, res) {
     res.status(200).json({ users: [
         { name: 'john' },
         { name: 'greg' },
-    ] })
+    ]})
   }
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,8 +1,22 @@
-export default function Users() {
+export default function Users({ users }) {
+  console.log(users)
+    let users_element = users.map(user => <p key={user.name}>{user.name}</p>)
     return (
     <div className="Users">
         <h1>Users</h1>
         <p>This is where all the users are listed</p>
+        {users_element}
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3000/api/users')
+  const data = await res.json()
+  const users = data.users
+  return {
+    props: {
+      users,
+    },
+  }
 }
